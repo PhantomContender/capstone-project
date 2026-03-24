4fab96fc9eda090a9b813bffe43d43a8cb247988
phantomcontender <adhoward86@yahoo.com>
2026-03-24 15:54:22 -0400

    implemented Role-Based Access Control and Admin gatekeeper

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -25,4 +25,16 @@ const protect = async (req, res, next) => {
   }
 };
 
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') 
+    {
+    next();
+  } 
+  else 
+    {
+    res.status(403);
+    throw new Error('Not authorized as an admin');
+  }
+};
+
 module.exports = { protect };

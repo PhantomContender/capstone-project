ee0e106797ccdcda56ec4e8f1bf90a2b9fb0f014
phantomcontender <adhoward86@yahoo.com>
2026-04-08 11:41:36 -0400

    fixing errors

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://zenith-wellness-spa.onrender.com/api',
+  baseURL: 'https://zenith-wellness.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {

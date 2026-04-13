c9f91e6279ab8950c764a4123ae913515dcfebc8
phantomcontender <adhoward86@yahoo.com>
2026-04-13 15:07:59 -0400

    updated to allow new user registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,8 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-exports.register = async (req, res) =>
-    {
+exports.register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
 
@@ -12,13 +11,10 @@ exports.register = async (req, res) =>
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password, 
       role
     });
 
@@ -48,7 +44,7 @@ exports.login = async (req, res) =>
       return res.status(401).json({ message: 'Invalid credentials' });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await user.matchPassword(password);
 
     if (!isMatch) {
       return res.status(401).json({ message: 'Invalid credentials' });
